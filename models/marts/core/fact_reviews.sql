b0c3d13c7540a2526e318fabf806cdd353c7fd76
EduPucheta <edugpucheta@gmail.com>
2026-01-21 13:22:04 -0300

    feat: Create `fact_reviews` model by joining reviews, surveys, and user dimensions.n y fact table

--- models/marts/core/fact_reviews.sql
+++ models/marts/core/fact_reviews.sql
@@ -3,7 +3,8 @@
 select
     r.survey,
     r.rating,
-    s.user_id
+    s.user_id,
+    u.plan
 from {{ ref('stg_reviews') }} r
 left join {{ ref('stg_surveys') }} s on cast(r.survey as string) = cast(s.survey_id as string)
-
+left join {{ ref('dim_users') }} u on s.user_id = u.user_id
